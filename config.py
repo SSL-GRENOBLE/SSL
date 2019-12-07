3c3ee4cf0e281da746107fe975f8dd01a1499445
Vladislav Shlyonskiy <vladislav.shlyonskiy@phystech.edu>
2019-12-08 00:04:53 +0100

    Config and LDA fixes

--- config.py
+++ config.py
@@ -9,18 +9,26 @@ sys.path.append(sslearn_root)
 
 # Import algorithm to test.
 from sslearn.models.sla._customs import BinarySLARandomForestClassifier  # noqa
+from sslearn.models.lda import (
+    SemiSupervisedLinearDiscriminantAnalysis,
+    LinearDiscriminantAnalysis,
+)
 
 
 configs = {
-    "sla": {
-        "model_cls": BinarySLARandomForestClassifier,
-        "baseline_cls": RandomForestClassifier,
-        "model_inits": {
-            "n_estimators": 100,
-            "adaptive": True,
-            "margin_mode": "soft",
-            "max_iter": 150,
-        },
-        "baseline_inits": {"n_estimators": 100},
-    }
+    # "sla": {
+    #     "model_cls": BinarySLARandomForestClassifier,
+    #     "baseline_cls": RandomForestClassifier,
+    #     "model_inits": {
+    #         "n_estimators": 100,
+    #         "adaptive": True,
+    #         "margin_mode": "soft",
+    #         "max_iter": 150,
+    #     },
+    #     "baseline_inits": {"n_estimators": 100},
+    # },
+    "lda": {
+        "model_cls": SemiSupervisedLinearDiscriminantAnalysis,
+        "baseline_cls": LinearDiscriminantAnalysis
+    },
 }
