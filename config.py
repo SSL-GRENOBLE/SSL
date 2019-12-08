a5484eab26b25bdb35498acc34f83556074d41f6
Dmitrii Borisov <abdima2010@mail.ru>
2019-12-08 01:35:59 +0100

    fix config

--- config.py
+++ config.py
@@ -16,17 +16,17 @@ from sslearn.models.lda import (
 
 
 configs = {
-    # "sla": {
-    #     "model_cls": BinarySLARandomForestClassifier,
-    #     "baseline_cls": RandomForestClassifier,
-    #     "model_inits": {
-    #         "n_estimators": 100,
-    #         "adaptive": True,
-    #         "margin_mode": "soft",
-    #         "max_iter": 150,
-    #     },
-    #     "baseline_inits": {"n_estimators": 100},
-    # },
+    "sla": {
+        "model_cls": BinarySLARandomForestClassifier,
+        "baseline_cls": RandomForestClassifier,
+        "model_inits": {
+            "n_estimators": 100,
+            "adaptive": True,
+            "margin_mode": "soft",
+            "max_iter": 150,
+        },
+        "baseline_inits": {"n_estimators": 100},
+    },
     "lda": {
         "model_cls": SemiSupervisedLinearDiscriminantAnalysis,
         "baseline_cls": LinearDiscriminantAnalysis
