0f6202136fbb954385118d86a4d1af608f65b22b
rodinasophie <rodinasophie@gmail.com>
2019-12-16 20:52:26 +0100

    Fix (anti)causal mix

--- experimentarium/data_react/genfuncs/causal.py
+++ experimentarium/data_react/genfuncs/causal.py
@@ -1,11 +1,2 @@
-import numpy as np
-
-
-def generate_gaussian(**kwargs):
-    n_samples = 10
-    n_features = 2
-    n_classes = 2
-    return (
-        np.ones((n_samples, n_features)),
-        np.random.randint(0, n_classes, size=n_samples),
-    )
+def generate_sign(**kwargs):
+    pass
