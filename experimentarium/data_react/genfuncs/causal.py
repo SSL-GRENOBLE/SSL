411a3e600de1549c3b56b3c1ba8e7ccfe5d9b2fb
rodinasophie <rodinasophie@gmail.com>
2019-12-17 01:19:00 +0100

    Add synthetic causal data[1]

--- experimentarium/data_react/genfuncs/causal.py
+++ experimentarium/data_react/genfuncs/causal.py
@@ -1,30 +1,24 @@
 import numpy as np
 
 
-def generate_sign_nonlinear(**kwargs):
+def _generate_function(func, **kwargs):
     params = dict(kwargs.items())
     n_samples = params["n_samples"]
     max_value = params["range"]
+    eps = params["eps"]
 
     x = np.empty((n_samples, 1))
     y = np.empty((n_samples, ))
 
     for i in range(0, n_samples):
         x[i] = np.random.uniform(-max_value, max_value)
-        y[i] = 1 if np.sign(np.sin(x[i])) > 0 else 0
+        y[i] = 1 if np.sign(func(x[i] + eps)) > 0 else 0
     return (x, y)
+
+
+def generate_sign_nonlinear(**kwargs):
+    return _generate_function(np.sin, **kwargs)
 
 
 def generate_sign_linear(**kwargs):
-    params = dict(kwargs.items())
-    n_samples = params["n_samples"]
-    eps = params["eps"]
-    max_value = params["range"]
-
-    x = np.empty((n_samples, 1))
-    y = np.empty((n_samples, ))
-
-    for i in range(0, n_samples):
-        x[i] = np.random.uniform(-max_value, max_value)
-        y[i] = 1 if np.sign(x[i] + eps) > 0 else 0
-    return (x, y)
+    return _generate_function(lambda x: x, **kwargs)
