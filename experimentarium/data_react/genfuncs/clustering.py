9595219999f43c3f127f7a782da10bdf6ad6432d
Vladislav Shlyonskiy <vladislav.shlyonskiy@phystech.edu>
2019-12-18 11:22:23 +0100

    Style fix

--- experimentarium/data_react/genfuncs/clustering.py
+++ experimentarium/data_react/genfuncs/clustering.py
@@ -1,6 +1,4 @@
 import sklearn
-from pandas import DataFrame
-from sklearn.datasets import samples_generator
 
 
 def generate_moons(**kwargs):
@@ -8,8 +6,8 @@ def generate_moons(**kwargs):
     n_samples = params["n_samples"]
     noise = params["noise"]
 
-    X, y = sklearn.datasets.make_moons(n_samples=n_samples, noise=noise)
-    return (X, y)
+    x, y = sklearn.datasets.make_moons(n_samples=n_samples, noise=noise)
+    return x, y
 
 
 def generate_circles(**kwargs):
@@ -17,8 +15,8 @@ def generate_circles(**kwargs):
     n_samples = params["n_samples"]
     noise = params["noise"]
 
-    X, y = sklearn.datasets.make_circles(n_samples=n_samples, noise=noise)
-    return (X, y)
+    x, y = sklearn.datasets.make_circles(n_samples=n_samples, noise=noise)
+    return x, y
 
 
 def generate_quadratic(**kwargs):
@@ -27,9 +25,10 @@ def generate_quadratic(**kwargs):
     cluster_std = params["cluster_std"]
     centers = params["centers"]
 
-    X, y = sklearn.datasets.make_blobs(
-        n_samples=n_samples, centers=centers, shuffle=False, cluster_std=cluster_std)
+    x, y = sklearn.datasets.make_blobs(
+        n_samples=n_samples, centers=centers, shuffle=False, cluster_std=cluster_std
+    )
 
-    y[:n_samples // 2] = 0
+    y[: n_samples // 2] = 0
     y[n_samples // 2:] = 1
-    return (X, y)
+    return x, y
