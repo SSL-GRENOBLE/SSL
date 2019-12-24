19afd5c635eccee6b1ee5b72c7934e1a86052f99
rodinasophie <rodinasophie@gmail.com>
2019-12-24 18:14:57 +0100

    Add separate noise

--- experimentarium/data_react/genfuncs/causal.py
+++ experimentarium/data_react/genfuncs/causal.py
@@ -5,15 +5,16 @@ def _generate_with_function(func, **kwargs):
     params = dict(kwargs.items())
     n_samples = params["n_samples"]
     max_value = params["range"]
+    noise_limit = params["noise"]
 
     X = np.empty((n_samples, 2))
     y = np.empty((n_samples,))
-    noise = [np.random.normal(0, 1, n_samples), np.random.normal(0, 1, n_samples)]
+    noise = np.random.normal(0, noise_limit, n_samples)
 
     for i in range(0, n_samples):
-        X[i][0] = np.random.uniform(-max_value, max_value) + noise[0][i]
-        X[i][1] = np.random.uniform(-max_value, max_value) + noise[1][i]
-        y[i] = 1 if np.sign(func(X[i][0] + X[i][1])) > 0 else 0
+        X[i][0] = np.random.uniform(-max_value, max_value)
+        X[i][1] = np.random.uniform(-max_value, max_value) 
+        y[i] = 1 if np.sign(func(X[i][0] + X[i][1]) + noise[0]) > 0 else 0
     return (X, y)
 
 
