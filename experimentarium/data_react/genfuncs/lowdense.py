42c374872699a291b32fc67771e03771f939068d
MargaritaKotova <ritak2404@gmail.com>
2020-01-03 15:16:11 +0300

    Kotova/LowDensity: change no_lowdense dataset

--- experimentarium/data_react/genfuncs/lowdense.py
+++ experimentarium/data_react/genfuncs/lowdense.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from .defaults import RANDOM_STATE
-
+from matplotlib import pyplot as plt
 
 def _get_random_data():
     r = np.random.RandomState(RANDOM_STATE)
@@ -20,8 +20,11 @@ def generate_vertical_no_lowdense(**kwargs):
 def generate_no_lowdense(**kwargs):
     x = _get_random_data()
     x = x[np.abs(x[:, 0]) >= 0.15]
-    x = x[np.abs(x[:, 1]) >= 0.15]
-    y = np.logical_xor(x[:, 0] > 0, x[:, 1] > 0).astype("int")
+    y = np.logical_or(
+        np.logical_and(x[:, 0] > 0, x[:, 1] > 0),
+        np.logical_and(x[:, 0] < 0, x[:, 1] > 0)
+    ).astype("int")
+    plt.scatter(x[:, 0], x[:, 1], c=y)
     return x, y
 
 
